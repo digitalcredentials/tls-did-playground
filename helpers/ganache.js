de7b9436a8660ef8eb444f8267744b150c97bf03
Micha Lutz <micha.lutz@tum.de>
2020-11-20 11:01:48 +0100

    fix-ganache-script

--- helpers/ganache.js
+++ helpers/ganache.js
@@ -1,9 +1,6 @@
-import environment from '../environment.json';
-import { execSync } from 'child_process';
+import environment from "../environment.json";
+import { execSync } from "child_process";
 
-execSync(
-  `npx ganache-cli --account '${environment.privateKey}, 100000000000000000000'`,
-  {
-    stdio: 'inherit',
-  }
-);
+execSync(`npx ganache-cli --account="${environment.privateKey},100000000000000000000"`, {
+  stdio: "inherit",
+});
