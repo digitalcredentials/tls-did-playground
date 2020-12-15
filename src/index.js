c016cb6687eb5ff69f9942144b1bc70d46d12f23
Kilian Käslin <kilian.kaeslin@tum.de>
2020-12-15 15:02:28 +0100

    - Moved to new signing process (pass key at each contract change) - Renamed registerChain to addChain - Added node reload script

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const pemKey = readFileSync(__dirname + pemKeyPath, 'utf8');
 console.log('TLS pem key: \n', `${pemKey.substring(0, 64)}...`);
 
 //Setup TLSDID object
-const tlsDid = new TLSDID(pemKey, etherPrivateKey, {
+const tlsDid = new TLSDID(etherPrivateKey, {
   registry: REGISTRY,
   providerConfig: {
     rpcUrl: jsonRpcUrl,
@@ -48,7 +48,7 @@ console.log('Contract address:', tlsDid.getAddress());
 //for the same domain during one test session
 const domain = 'tls-did.de';
 console.log('Registering contract with domain:', domain);
-await tlsDid.registerContract(domain);
+await tlsDid.registerContract(domain, pemKey);
 
 //Register TLS pem cert chain
 //Registering is needed for the full chain exept the root certificate
@@ -57,21 +57,24 @@ const cert = readFileSync(__dirname + certPath, 'utf8');
 const intermediateCertPath = '/ssl/certs/intermediateCert.pem';
 const intermediateCert = readFileSync(__dirname + intermediateCertPath, 'utf8');
 const chain = [cert, intermediateCert];
-console.log('Registering cert chain:', chain);
-await tlsDid.registerChain(chain);
+console.log(
+  'Adding cert chain:',
+  chain.map((cert) => `${cert.substring(0, 64)}...`)
+);
+await tlsDid.addChain(chain, pemKey);
 
 //Add attributea to DID Document (path, value)
 console.log('Adding example attribute to DID Document');
 //Adds {parent: {child: value}}
-await tlsDid.addAttribute('parent/child', 'value');
+await tlsDid.addAttribute('parent/child', 'value', pemKey);
 //Adds {array: [{element: value}]}
-await tlsDid.addAttribute('arrayA[]/element', 'value');
+await tlsDid.addAttribute('arrayA[]/element', 'value', pemKey);
 //Adds {array: [value]}
-await tlsDid.addAttribute('arrayB[]', 'value');
+await tlsDid.addAttribute('arrayB[]', 'value', pemKey);
 
 //Add expiry to tls DID contract
 console.log('Setting expiry');
-await tlsDid.setExpiry(new Date('2040/12/12'));
+await tlsDid.setExpiry(new Date('2040/12/12'), pemKey);
 
 //Resolve DID Document
 console.log('Resolving DID Document for did:', `did:tls:${domain}`);
@@ -82,5 +85,9 @@ const resolver = getResolver(
   REGISTRY,
   rootCertificates
 );
-const didDocument = await resolver.tls(`did:tls:${domain}`);
-console.log('DID Document:', didDocument);
+try {
+  const didDocument = await resolver.tls(`did:tls:${domain}`);
+  console.log('DID Document:', didDocument);
+} catch (err) {
+  console.error('Error while resolving did.', err.message);
+}
